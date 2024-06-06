848fa47264dfa684c48c1c76693b7f3ca052857b
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-06 08:35:26 +0700

    add about page

---
+++ src/page/About.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import Nav from '../components/Nav'
+import Jumbotron from '../components/Jumbotron'
+
+const AboutPage = () => {
+    return (
+        <>
+            <Nav />
+            <h1>
+                this is anoyt
+            </h1>
+            <h1>
+                this is anoyt
+            </h1>
+            <h1>
+                this is anoyt
+            </h1>
+            <h1>
+                this is anoyt
+            </h1>
+        </>
+        
+    );
+}
+
+export default AboutPage;
