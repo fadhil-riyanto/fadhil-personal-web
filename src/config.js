a44456e815a24d5f0bc5a27571886810862c3972
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-06 08:35:47 +0700

    add config file for links storing

---
+++ src/config.js
@@ -0,0 +1,67 @@
+const config = {
+    BASE_URL: "https://fadev.org",
+    SITENAME: "Fadhil DEV"
+}
+
+const config_dropdown_links = [
+    {
+        name: "API",
+        links: "https://api.fadev.org"
+    },
+    {
+        name: "Book",
+        links: "/ebook"
+    },
+    {
+        name: "Music",
+        links: "/ebook"
+    }
+    // ,
+    // {
+    //     name: "Shortlink",
+    //     links: "s.fadev.org"
+    // }
+]
+
+const config_navbar_links = [
+    {
+        name: "Blog",
+        links: "/blog"
+    },
+    {
+        name: "About",
+        links: "/about"
+    },
+    {
+        name: "Links",
+        links: "/links"
+    },
+    {
+        name: "Projects",
+        links: "/projects"
+    },
+    {
+        name: "Contact",
+        links: "/contact"
+    }
+]
+
+const config_footer_links = [
+    {
+        name: "Telegram",
+        links: "https://fadhil_riyanto.t.me"
+    },
+    {
+        name: "Github",
+        links: "https://github.com/fadhil-riyanto"
+    },
+    {
+        name: "Google developer",
+        links: "https://developers.google.com/profile/u/fadhil-riyanto"
+    }
+]
+
+
+export {
+    config, config_dropdown_links, config_navbar_links, config_footer_links
+}
