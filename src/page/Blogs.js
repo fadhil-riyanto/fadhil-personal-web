bb7ba2677ad0e28a574267151d6fd76ed58b06a5
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-07 19:11:06 +0700

    add article list function

--- src/page/Blogs.js
+++ src/page/Blogs.js
@@ -32,6 +32,26 @@ const BlogHeading = () => {
     )
 }
 
+const BlogArticleCard = () => {
+    return (
+        <>
+            <div className="col-lg-3 col-md-4 col-sm-6 col-12 px-md-1 px-4">
+                <Link to="/blog/hello-world" className="fadhil_r_bg_color m-2 text-decoration-none">
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/PSP-Homebrew.jpeg/800px-PSP-Homebrew.jpeg' className='mx-auto d-block img-fluid' ></img>
+                    {/* <svg className="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> */}
+                    <div className="card-body fadhil_r_bg_color">
+                        <p className="card-text text-white">
+                            <h2>ssjj</h2>
+                            This tutorial explains how to install Docker on Ubuntu. We'll install the latest Docker package from the official Docker’s repositories..
+                        </p>
+                    
+                    </div>
+                </Link>
+            </div>
+        </>
+    )
+}
+
 const BlogPage = () => {
     return (
         <>
@@ -39,6 +59,16 @@ const BlogPage = () => {
             <div class="fadhil_r_bg_color">
                 <div className='py-4 min-vh-100'>
                 <BlogHeading />
+                <div className='container'>
+                    <div className="row mt-4">
+                        <BlogArticleCard />
+                        <BlogArticleCard />
+                        <BlogArticleCard />
+                        <BlogArticleCard />
+                        <BlogArticleCard />
+                    </div>
+                    
+                </div>
                 </div>
             </div>
         </>
