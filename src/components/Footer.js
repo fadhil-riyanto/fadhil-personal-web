e0d85f923d38b8e1da81f808f7c1108257b97f1f
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-09 23:35:32 +0700

    enchanted links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,30 +16,30 @@ const Footer = () => {
         <>
             <footer className="w-100 py-4 bg-dark flex-shrink-0">
                 <div className="container py-4">
-                    <div className="row gy-4 gx-5">
-                    <div className="col-lg-4 col-md-6">
-                        <h5 className="h1 text-white">
-                            Fadhil DEV
-                        </h5>
-                        <p className="small text-white">
-                            {config.BIO}
-                        </p>
-                        <p className="small text-white mb-0">
-                        © Copyrights. All rights reserved.{" "}
-                        <a className="text-primary" href="#">
-                            fadev.org
-                        </a>
-                        </p>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <h5 className="text-white mb-3">Links</h5>
-                        <ul className="list-unstyled text-muted">
-                            {config_connect_links.map((item) => 
-                                <FooterList name={item.name} links={item.links} key={item.links}/>
-                            )}
-                            
-                        </ul>
-                    </div>
+                    <div className="row ">
+                        <div className="col-lg-4 col-md-6 order-2 order-lg-1">
+                            <h5 className="h1 text-white">
+                                Fadhil DEV
+                            </h5>
+                            <p className="small text-white">
+                                {config.BIO}
+                            </p>
+                            <p className="small text-white mb-0">
+                            © Copyrights. All rights reserved.{" "}
+                            <a className="text-primary" href="#">
+                                fadev.org
+                            </a>
+                            </p>
+                        </div>
+                        <div className="col-lg-4 col-md-6 order-1 order-lg-2">
+                            <h5 className="text-white mb-3">Links</h5>
+                            <ul className="list-unstyled text-muted">
+                                {config_connect_links.map((item) => 
+                                    <FooterList name={item.name} links={item.links} key={item.links}/>
+                                )}
+                                
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </footer>
