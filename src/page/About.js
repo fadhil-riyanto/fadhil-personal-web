d44d7245c34c67abec435c3d3836e639dd6cedd3
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-11 07:30:16 +0700

    add skills

--- src/page/About.js
+++ src/page/About.js
@@ -1,5 +1,7 @@
 import Nav from '../components/Nav'
-import { config_skills_level } from '../config'
+import { config_skills_level, config_tools_level } from '../config'
+import '../components/css/Custom.css'
+import { useState } from 'react'
 
 const HeaderSection = ({text}) => {
     return (
@@ -13,26 +15,53 @@ const HeaderSection = ({text}) => {
     )
 }
 
-const Skillcard = (logo) => {
+const SkillCard = ({logo, name, level}) => {
     return (
         <>
-            <div className='col-lg-3 col-md-4 col-6 border border-warning text-white row'>
-                <div className='col-2'>
-                    <img src={logo}></img>
+            <div className='col-lg-3 col-md-4 col-6 text-white'>
+                <div className='border border-warning m-1 p-1 row fadhil_r_hoverable'>
+                    <div className='col-3'>
+                        <img src={logo} style={{width: "100%"}}></img>
+                    </div>
+                    <div className='col-9 d-flex flex-column'>
+                        <div>
+                            {name}
+                        </div>
+                        <div >
+                            {level}
+                        </div>
+                    </div>
                 </div>
+                
             </div>
         </>
     )
 }
 
 const SkillistTable = () => {
+    
     return (
         <>
             <div className='row'>
-                {config_skills_level.map((data) => {
-                    console.log(data.logo);
-                    <Skillcard logo={data.logo}/>
-                })}
+                {/* <Skillcard logo="aaa"/> */}
+                {config_skills_level.map((data) => 
+                    <SkillCard logo={data.logo} name={data.name} level={data.level}/>
+                )}
+            </div>
+
+        </>
+    )
+}
+
+const ToolsTable = () => {
+    
+    return (
+        <>
+            <div className='row'>
+                {/* <Skillcard logo="aaa"/> */}
+                {config_tools_level.map((data) => 
+                    <SkillCard logo={data.logo} name={data.name} level={data.level}/>
+                )}
             </div>
 
         </>
@@ -40,6 +69,7 @@ const SkillistTable = () => {
 }
 
 const Skills = () => {
+    
     return (
         <>
         {/* fadhil_r_bg_color */}
@@ -47,6 +77,8 @@ const Skills = () => {
                 <div className="container-fluid">
                     <HeaderSection text="Skills"/>
                     <SkillistTable />
+                    <HeaderSection text="Tools"/>
+                    <ToolsTable />
                 </div>
             </div>
 
@@ -65,10 +97,10 @@ const Aboutme = () => {
                         <div className="col-lg-9 order-2 order-lg-1">
                             <p className="fs-4 text-white">
                                 I am Fadhil Riyanto, I love programming and math (including science), Im from semarang (hot enough).
-                                im currenly studying about Linux kernel, low level machine, and website user interface
+                                im currenly studying about Linux kernel, low level machine, and website user interface.
                                 I'm really passionate about Backend programming and HPC infrastructure.
                                 One of my goals is have dedicated server on my room, have domain and hosting bussines,
-                                and work fulltime as programmer
+                                work fulltime as programmer, and become linux kernel maintainer.
 
                                 fun fact, I like working from behind, exposed to people is too tiring
                             </p>
@@ -85,6 +117,7 @@ const Aboutme = () => {
 }
 
 const AboutPage = () => {
+    
     return (
         <>
             <Nav />
