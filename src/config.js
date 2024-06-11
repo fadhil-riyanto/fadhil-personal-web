0c966a103c05c8c764764449751a1c2d06025e02
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-11 07:29:36 +0700

    add skills set

--- src/config.js
+++ src/config.js
@@ -36,21 +36,96 @@ const config_skills_level = [
         logo: BlogHelper.Asset("115px-Laravel.svg.png"),
         name: "Laravel", 
         level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("jsIconGreen.svg"),
+        name: "NodeJS", 
+        level: "Intermediate"
+    },
+    {
+        logo: BlogHelper.Asset("1920px-Go_Logo_Blue.svg.png"),
+        name: "Go", 
+        level: "Intermediate"
+    },
+    {
+        logo: BlogHelper.Asset("dotnet-logo.svg"),
+        name: ".NET (server)", 
+        level: "Intermediate"
+    },
+    {
+        logo: BlogHelper.Asset("python-logo-only.png"),
+        name: "Python (server)", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("css.png"),
+        name: "CSS", 
+        level: "Beginner"
+    },
+    {
+        logo: BlogHelper.Asset("javascript_original_logo_icon_146455.png"),
+        name: "JS (browser)", 
+        level: "Beginner"
+    },
+    {
+        logo: BlogHelper.Asset("bootstrap-logo-shadow.png"),
+        name: "Bootstrap", 
+        level: "Intermediate"
+    },
+    {
+        logo: BlogHelper.Asset("React.webp"),
+        name: "React", 
+        level: "Beginner"
+    },
+    {
+        logo: BlogHelper.Asset("ISO_C++_Logo.svg.png"),
+        name: "C++", 
+        level: "Intermediate"
+    },
+    {
+        logo: BlogHelper.Asset("The_C_Programming_Language_logo.svg.png"),
+        name: "C", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("HHVM_logo.svg.png"),
+        name: "HHVM", 
+        level: "Advanced"
     }
-    // ,
-    // {
-    //     name: "Laravel", 
-    //     level: "Advanced"
-    // },
-    // ,
-    // {
-    //     name: "Node", 
-    //     level: "Intermediate"
-    // },
-    // {
-    //     name: "Go", 
-    //     level: "Intermediate"
-    // },
+    // 
+]
+
+const config_tools_level = [
+    {
+        logo: BlogHelper.Asset("Git-Icon-1788C.png"),
+        name: "GIT", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("archlinuxlogo.square.png"),
+        name: "ArchLinux", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("mariadb-logo-vertical_white.svg"),
+        name: "MariDB", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("redis.png"),
+        name: "Redis", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("png-transparent-mongodb-original-wordmark-logo-icon.png"),
+        name: "MongoDB", 
+        level: "Advanced"
+    },
+    {
+        logo: BlogHelper.Asset("png-transparent-mongodb-original-wordmark-logo-icon.png"),
+        name: "MongoDB", 
+        level: "Advanced"
+    }
 ]
 
 const config_navbar_links = [
@@ -96,5 +171,5 @@ const config_connect_links = [
 
 
 export {
-    config, config_dropdown_links, config_navbar_links, config_connect_links, config_skills_level
+    config, config_dropdown_links, config_navbar_links, config_connect_links, config_skills_level, config_tools_level
 }
